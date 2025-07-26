1380d3d9867f8d825e4e6657607ff42af16b2970
MrAndreID <MrAndreID@outlook.com>
2025-07-26 23:43:18 +0700

    add custom http error for echo framework

--- go.mod
+++ go.mod
@@ -2,15 +2,25 @@ module github.com/MrAndreID/gopackage
 
 go 1.24
 
-require github.com/go-playground/validator/v10 v10.27.0
+require (
+	github.com/go-playground/validator/v10 v10.27.0
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cast v1.9.2
+)
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- validator.go
+++ validator.go
@@ -1,7 +1,14 @@
 package gopackage
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cast"
 )
 
 // Custom Validator
@@ -16,4 +23,33 @@ func CustomValidator() *CustomValidatorData {
 
 func (cvd *CustomValidatorData) Validate(i any) error {
 	return cvd.validator.Struct(i)
+}
+
+// Custom HTTP Error for Echo Framework
+
+type EchoCustomHTTPErrorResponse struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+	Data        any    `json:"data"`
+	Internal    error  `json:"-"`
+}
+
+func EchoCustomHTTPErrorHandler(err error, c echo.Context) {
+	report, ok := err.(*echo.HTTPError)
+
+	if !ok {
+		report = echo.NewHTTPError(http.StatusInternalServerError, strings.ToUpper(strings.ReplaceAll(http.StatusText(http.StatusInternalServerError), " ", "_")))
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"tag": "GoPackage.EchoCustomHTTPErrorHandler.01",
+	}).Error(strings.ToLower(cast.ToString(report.Message)))
+
+	c.Logger().Error(report)
+
+	c.JSON(report.Code, EchoCustomHTTPErrorResponse{
+		Internal:    report.Internal,
+		Code:        fmt.Sprintf("%04d", report.Code),
+		Description: strings.ToUpper(strings.ReplaceAll(cast.ToString(report.Message), " ", "_")),
+	})
 }
